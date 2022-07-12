5a5fc33377f7dec8f541b2d5029da743a5fef16c
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-07-11 22:40:11 -0300

    Styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button variant="danger"/>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+    </>
   )
 }
